412b0c4645834aa43085ccbe2d0fcef7aad8204f
Somsubhra Bairi <somsubhra.b@endurance.com>
2016-02-02 13:28:44 +0530

    Save data

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from etc import Config
+import MySQLdb as mdb
 
 
 app = Flask(__name__)
@@ -21,8 +22,29 @@ def form_handler():
 
 @app.route("/save", methods=["POST"])
 def save_handler():
-    name = request.form["name"]
-    email = request.form["email"]
-    birthday = request.form["birthday"]
+    name = str(request.form["name"])
+    email = str(request.form["email"])
+    birthday = str(request.form["birthday"])
+
+    if name.strip() != "" and email.strip() != "" and birthday.strip() != "":
+        db_config = Config.db()
+        db_conn = mdb.connect(db_config["host"],
+                              db_config["user"],
+                              db_config["password"],
+                              db_config["name"])
+
+        db_cursor = db_conn.cursor()
+
+        query = "INSERT IGNORE INTO users(name, email, birthday) VALUES(%s, %s, %s)"
+
+        try:
+            db_cursor.execute(query, (name,
+                                      email,
+                                      birthday))
+            db_conn.commit()
+        except Exception as e:
+            return render_template("form.html", error=e.message)
+    else:
+        return render_template("form.html", error="All fields are required")
 
     return render_template("thanks.html")
